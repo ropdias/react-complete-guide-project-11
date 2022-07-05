86c9513c20c9932d47a85f29bc395c76bb9d8af9
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-07-04 23:10:22 -0300

    Building a Custom HTTP Hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,37 @@
+import { useState, useCallback } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.header ? requestConfig.header : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []); // We can remove the requestConfig and applyData of the dependencies because
+  // they are now parameters of the function not external dependencies
+
+  return {
+    isLoading, // Same as isLoading: isLoading
+    error, // Same as error: error
+    sendRequest, // Same as sendRequest: sendRequest
+  };
+};
+
+export default useHttp;
